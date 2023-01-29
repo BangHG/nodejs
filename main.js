743596522e8b1d4682ec66b614bd555ab43eeb4a
BangHG <banghgoff@gmail.com>
2023-01-29 11:56:12 +0900

    nodejs 13 App제작: 동적인 웹페이지 만들기

--- main.js
+++ main.js
@@ -1,15 +1,49 @@
 ﻿var http = require('http');
 var fs = require('fs');
+var url = require('url'); // url모듈
+
 var app = http.createServer(function (request, response) {
-  var url = request.url;
-  if (request.url == '/') {
-    url = '/index.html';
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
+
+  if (_url == '/') {
+    // _url = '/index.html';
+    title = 'WELCOME HOME';
   }
-  if (request.url == '/favicon.ico') {
+  if (_url == '/favicon.ico') {
     return response.writeHead(404);
   }
+
   response.writeHead(200);
-  response.end(fs.readFileSync(__dirname + url));
-  console.log(__dirname + url);
+
+  fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, content) {
+    var template = `
+    <!doctype html>
+    <html>
+    <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+    </head>
+    <body>
+    <h1><a href="/">WEB</a></h1>
+    <ul>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+    </ul>
+    <h2>${title}</h2>
+    <div>${content}</div>
+    </body>
+    </html>
+    `;
+    response.end(template);
+  });
+  // response.end(queryData.id); //쿼리스트링 출력하기!
+  //읽어주기
+  // response.end(fs.readFileSync(__dirname + _url));
+
+  // __dirname: 현재경로
+  console.log(__dirname + _url);
 });
 app.listen(3000);
