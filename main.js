5afa940bf53cca9f30d855da321361480da4148b
BangHG <banghgoff@gmail.com>
2023-01-29 19:03:47 +0900

    26 함수를 이용해서 정리정돈하기

--- main.js
+++ main.js
@@ -1,41 +1,54 @@
-﻿var http = require('http');
+var http = require('http');
 var fs = require('fs');
 var url = require('url'); // url모듈
+
+function templateHTML(title, list, body) {
+  return `
+  <!doctype html>
+  <html>
+  <head>
+  <title>WEB1 - ${title}</title>
+  <meta charset="utf-8">
+  </head>
+  <body>
+  <h1><a href="/">WEB</a></h1>
+  ${list} 
+  ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist) {
+  var list = '<ul>';
+  var i = 0;
+  while (i < filelist.length) {
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i = i + 1;
+  }
+  list = list + '</ul>';
+  return list;
+}
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
   var pathname = url.parse(_url, true).pathname;
-  console.log(url.parse(_url, true));
+  // console.log(url.parse(_url, true));
 
   if (pathname === '/') {
     if (queryData.id === undefined) {
       fs.readdir('./data', (err, filelist) => {
-        // console.log(filelist);
         var title = 'WELCOME HOME';
         var description = 'hello node.js';
-        var list = '<ul>';
-        var i = 0;
-        while (i < filelist.length) {
-          list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-          i = i + 1;
-        }
-        list = list + '</ul>';
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list} 
-        <h2 style="margin:0;padding:0">${title}</h2>      
-        <div style="margin:0;padding:0">${description}</div>
-        </body>
-        </html>
-        `;
+
+        var list = templateList(filelist);
+
+        var template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2>      
+          <div>${description}</div>`
+        );
         response.writeHead(200);
         response.end(template);
       });
@@ -43,28 +56,13 @@ var app = http.createServer(function (request, response) {
       fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
         fs.readdir('./data', (err, filelist) => {
           var title = queryData.id;
-          var list = '<ul>';
-          var i = 0;
-          while (i < filelist.length) {
-            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-            i = i + 1;
-          }
-          list = list + '</ul>';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-          </head>
-          <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list} 
-          <h2 style="margin:0;padding:0">${title}</h2>      
-          <div style="margin:0;padding:0">${description}</div>
-          </body>
-          </html>
-          `;
+          var list = templateList(filelist);
+          var template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2>      
+              <div>${description}</div>`
+          );
           response.writeHead(200);
           response.end(template);
         });
@@ -75,20 +73,5 @@ var app = http.createServer(function (request, response) {
     response.writeHead(404);
     response.end('Not Found');
   }
-
-  // if (_url == '/') {
-  //   // _url = '/index.html';
-  //   title = 'WELCOME HOME';
-  // }
-  // if (_url == '/favicon.ico') {
-  //   return response.writeHead(404);
-  // }
-
-  // response.end(queryData.id); //쿼리스트링 출력하기!
-  //읽어주기
-  // response.end(fs.readFileSync(__dirname + _url));
-
-  // __dirname: 현재경로
-  // console.log(__dirname + _url);
 });
 app.listen(3000);
