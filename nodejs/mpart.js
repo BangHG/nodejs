ce7f533bd79af61ea5507d70dbed1e5a10a1a0d5
BangHG <banghgoff@gmail.com>
2023-01-30 23:08:33 +0900

    45 모듈

---
+++ nodejs/mpart.js
@@ -0,0 +1,13 @@
+﻿// 모듈! 객체보다 더 큰 정리정돈.. 모듈
+
+var M = {
+  v: 'v',
+  f: function () {
+    console.log(this.v);
+  },
+};
+
+M.f();
+
+module.exports = M;
+//M이라는 객체를 외부에서 사용할수 있게 하겠습니다!
---
+++ nodejs/muse.js
@@ -0,0 +1,12 @@
+﻿// 모듈! 객체보다 더 큰 정리정돈.. 모듈
+
+// var M = {
+//   v: 'v',
+//   f: function () {
+//     console.log(this.v);
+//   },
+// };
+
+var part = require('./mpart.js');
+
+part.f();
